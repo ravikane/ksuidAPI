9acd3efbff726a893617ebdee56626310dad2b92
ravikane <nainglinnaung6871@gmail.com>
2023-07-15 19:36:19 +0630

    adding base codes for ksuid api

---
+++ index.js
@@ -0,0 +1,14 @@
+const KSUID = require("ksuid");
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
+
+app.get("/", (req, res) => {
+  const ksuidFromSync = KSUID.randomSync();
+  res.send(ksuidFromSync.string);
+});
